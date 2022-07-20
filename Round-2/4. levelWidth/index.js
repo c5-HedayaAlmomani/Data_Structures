4ff756642d94ffa404172cee48a3bae775a18cdd
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-07-20 16:22:06 +0300

    levelWidth

--- Round-2/4. levelWidth/index.js
+++ Round-2/4. levelWidth/index.js
@@ -15,9 +15,24 @@
 
 Answer: [1, 3, 2] 
 */
-
+//{root :data:data , chelidern=[]}
 const Node = require("./node");
 
-const levelWidth = (root) => {};
+const levelWidth = (root) => {
+  const arr = [root, "end level"];
+  const answer = [0];
+
+  while (arr.length > 1) {
+    let node = arr.shift();
+    if (node === "end level") {
+      answer.push(0);
+      arr.push("end level");
+    } else {
+      arr.push(...node.children);
+      answer[answer.length - 1]++;
+    }
+  }
+  return answer;
+};
 
 module.exports = levelWidth;
